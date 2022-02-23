61d309eb163295921868da97ead391c9ccf68f69
Jason <k.jason0105@gmail.com>
2022-02-22 17:16:37 -0800

    moved some files around

---
+++ sort/bubble_sort.js
@@ -0,0 +1,12 @@
+function bubbleSort(arr){
+  let sorted = false;
+
+  while (!sorted) {
+    sorted = true;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i+1]) {
+
+      }
+    }
+  }
+}
