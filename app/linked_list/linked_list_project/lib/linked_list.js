d87a66c8b995a669a6e4b335c9ecb8191a0eb2e3
Jason <k.jason0105@gmail.com>
2022-02-23 22:15:18 -0800

    add and remove tails for linked lists

--- app/linked_list/linked_list_project/lib/linked_list.js
+++ app/linked_list/linked_list_project/lib/linked_list.js
@@ -46,12 +46,32 @@ class LinkedList {
         } else {
             this.tail.next = newNode;
         }
+
+        this.tail = newNode;
+        this.length++;
         return this;
     }
 
     // TODO: Implement the removeTail method here
     removeTail() {
+        if (this.length === 0) {
+            return undefined;
+        }
+        let current = this.head;
+        let newTail = current;
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
 
+        if (this.length === 0) {
+            this.tail = null;
+            this.head = null;
+        }
+        return current;
     }
 
     // TODO: Implement the addToHead method here
