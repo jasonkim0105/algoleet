7a57e665f84be73bf68f779fc589a16e38bd895b
Jason <k.jason0105@gmail.com>
2022-03-02 21:45:47 -0800

    reverse linked list given list

--- app/linked_list/linked_list_interview_problems/lib/1_reverse_linked_list.js
+++ app/linked_list/linked_list_interview_problems/lib/1_reverse_linked_list.js
@@ -13,7 +13,7 @@
 //
 // ------------
 // Constraints:
-// ------------ 
+// ------------
 //
 // (1) Your function must run in linear time, O(n).
 // (2) You must reverse the list *in place*. (i.e. Use constant space, O(1).)
@@ -62,7 +62,23 @@
 // -----------
 function reverseLinkedList(linkedList) {
   // TODO: Implement the reverseLinkedList function!
+  // return linkedList;
+  let node = linkedList.head;
 
+  let first = node;
+  let next = null;
+  let prev = null;
+
+  while (next = node.next) {
+    node.next = prev;
+    prev = node;
+    node = next;
+  }
+
+  linkedList.head = node;
+  linkedList.head.next = prev;
+  linkedList.tail = first;
+  return linkedList;
 }
 
 // ----------------------------------------
