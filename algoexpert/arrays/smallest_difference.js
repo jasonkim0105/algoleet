6cdbb1e2516886f5e16cf750402ed64b14759caf
Jason <k.jason0105@gmail.com>
2022-03-09 22:23:05 -0800

    smallest difference not working entirely

---
+++ algoexpert/arrays/smallest_difference.js
@@ -0,0 +1,37 @@
+function smallestDifference(arrayOne, arrayTwo) {
+  // Write your code here.
+	let arrayOneSorted = arrayOne.sort((a, b) => a-b);
+	let arrayTwoSorted = arrayTwo.sort((a, b) => a-b);
+  let pointerOne = 0;
+  let pointerTwo = 0;
+  let current;
+  let smallest;
+  let answer = [];
+
+  while(pointerOne < arrayOneSorted.length && pointerTwo < arrayTwoSorted.length) {
+    let firstNum = arrayOneSorted[pointerOne];
+    let secondNum = arrayTwoSorted[pointerTwo];
+    if (firstNum < secondNum) {
+      current = firstNum - secondNum;
+      pointerOne++;
+    } else if (secondNum < firstNum) {
+      current = secondNum - firstNum;
+      pointerTwo++;
+    } else {
+      return [firstNum, secondNum]
+    }
+
+    if (smallest > current) {
+      smallest = current;
+      answer = [firstNum, secondNum]
+    }
+    return answer;
+  }
+
+}
+
+exports.smallestDifference = smallestDifference;
+
+arrayOne = [-1, 5, 10, 20, 28, 3]
+arrayTwo = [26, 134, 135, 15, 17]
+smallestDifference(arrayOne, arrayTwo)
