b6e190817978645c67ef641e85528cf1e8e35b38
Jason <k.jason0105@gmail.com>
2022-04-08 12:55:43 -0700

    climibng stairs

--- leetcode/dynamic programming/climbing_stairs.js
+++ leetcode/dynamic programming/climbing_stairs.js
@@ -24,3 +24,11 @@
 // Constraints:
 
 // 1 <= n <= 45
+
+var climbStairs = function(n) {
+  const memo = [1, 1, 2]
+  for (let i = 3; i <= n; i++) {
+      memo[i] = memo[i - 1] + memo[i-2]
+  }
+  return memo[n]
+};
