ef39f955d29bd1e9d74503e254209ddb30bf0ba9
Jason <k.jason0105@gmail.com>
2022-06-17 19:17:46 -0700

    diff method

--- leetcode/linked list/remove_nth_node_from_end.js
+++ leetcode/linked list/remove_nth_node_from_end.js
@@ -1,31 +1,75 @@
-// This is an input class. Do not edit.
-class LinkedList {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+// // This is an input class. Do not edit.
+// class LinkedList {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
 
-function removeKthNodeFromEnd(head, k) {
-  // Write your code here.
-  let current = head;
-  let count = 1;
-  while(current.next !== null) {
-    current = current.next;
-    count++;
+// function removeKthNodeFromEnd(head, k) {
+//   // Write your code here.
+//   let current = head;
+//   let count = 1;
+//   while(current.next !== null) {
+//     current = current.next;
+//     count++;
+//   }
+//   const val = count - k;
+//   // console.log(val)
+//   let currentNode = head;
+//   let prev = null;
+//   let counter = 0;
+//   while (currentNode.next !== null) {
+//     if (counter === val) {
+//       prev.next = currentNode.next
+//     }
+//     prev = currentNode
+//     currentNode = currentNode.next;
+//     counter ++;
+
+//   }
+// }
+
+
+var removeNthFromEnd = function(head, n) {
+  let slow = head;
+  let fast = head;
+
+  for (let i = 0; i < n; i++) {
+      fast = fast.next;
   }
-  const val = count - k;
-  // console.log(val)
-  let currentNode = head;
-  let prev = null;
-  let counter = 0;
-  while (currentNode.next !== null) {
-    if (counter === val) {
-      prev.next = currentNode.next
-    }
-    prev = currentNode
-    currentNode = currentNode.next;
-    counter ++;
+
+  while (fast && fast.next !== null) {
+      slow = slow.next;
+      fast = fast.next;
+  }
+  if (!fast) {
+      head = head.next
+  } else {
+      slow.next = slow.next.next
 
   }
-}
+  return head
+
+//     let fast = head;
+//     let slow = null;
+//     let prev = null;
+//     let count = 1;
+//     while (fast !== null) {
+
+//         if (count === n + 1) {
+//             slow = head
+//         }
+
+//         if (slow) {
+//             prev = slow;
+//             slow = slow.next;
+//         }
+//         count++
+//         fast = fast.next;
+//     }
+
+//     if (slow === null) return head.next;
+//     prev.next = slow.next;
+//     return head
+};
